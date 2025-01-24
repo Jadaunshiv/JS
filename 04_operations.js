ced8f5ad38e9eaa667ca9217e2185240ebc8bd45
Jadaunshiv <163510201+Jadaunshiv@users.noreply.github.com>
2025-01-24 17:06:21 +0000

    initial operations commit

---
+++ 04_operations.js
@@ -0,0 +1,7 @@
+// console.log(2 + 3) //addition operation
+// console.log(2 - 3) //subtraction operation
+// console.log(2 * 3) //multiplication operation
+// console.log(6 / 3) //division operation(gives quotient)
+// console.log(6 % 3) //modulo operation(gives remainder)
+// console.log(2 ** 3) //power operation
+
