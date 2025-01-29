4e975c6be85b0263bbf3e3406cc19d9c52dc86f4
Jadaunshiv <163510201+Jadaunshiv@users.noreply.github.com>
2025-01-29 12:57:58 +0000

    initial 08_dates_time.js commit

---
+++ 01_basics/08_dates_time.js
@@ -0,0 +1,15 @@
+//Dates
+
+let todaysDate = new Date() //type of date is OBJECT
+// console.log(todaysDate)
+// console.log(todaysDate.toLocaleString())
+// console.log(todaysDate.toDateString())
+
+// let randomDate = new Date(2024, 5, 15, 7, 6)
+// let randomDate = new Date("01-15-2024")
+// console.log(randomDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp.toLocaleString());
+
+
