83c490afef396064d9d48953cb0d619e250cec6a
Jadaunshiv <163510201+Jadaunshiv@users.noreply.github.com>
2025-02-01 09:06:45 +0000

    initial 04_objects.js commit

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,42 @@
+//nesting in objects
+
+let user = {
+    userName: {
+        fullName: {
+            firstname: "Shivam",
+            lastname: "Jadaun"
+        }
+    }
+}
+
+console.log(user.userName.fullName);
+
+let user2 = {
+    name: "abc",
+    "fullname": "abc def",
+    age: 24,
+    email: "abc@gmail.com",
+    isLoggedIn: true,
+    lastLoginDays: ["Monday", "Thursday", "Friday"]
+}
+
+
+//combining objects
+
+// let obj1 = {1: "a", 2: "b"}
+// let obj2 = {3: "a", 4: "b"}
+
+// let obj3 = Object.assign({}, obj1, obj2) //method 1 using assign()
+// let obj4 = {...obj1, ...obj2} //method 2 using spread operator
+// console.log(obj3);
+// console.log(obj4);
+
+console.log(Object.keys(user2)); //logs out keys present in the object
+console.log(Object.values(user2)); //logs out values of the keys of the object
+console.log(Object.entries(user2)); //logs out keys and it's values in a separate array
+console.log(user2.hasOwnProperty('isLoggedIn')); //tells whether a property is available in object or not
+
+
+
+
+
